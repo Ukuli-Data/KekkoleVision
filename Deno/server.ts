48abf08d22a33d184aee9de48a638adc41b9aced
Tomi Toivio <tomi.toivio@ukuli.fi>
2021-03-06 15:43:02 +0200

    Deno
    Co-Authored-By: Ukuli Data <info@ukuli.fi>
    Co-Authored-By: Tomi Toivio <tomi.toivio@ukuli.fi>
    Co-Authored-By: keuranos <2562975+keuranos@users.noreply.github.com>

--- Deno/server.ts
+++ Deno/server.ts
@@ -1,7 +1,7 @@
-const hostname = "0.0.0.0";
-const port = 8080;
-const listener = Deno.listen({ hostname, port });
-console.log(`Listening on ${hostname}:${port}`);
-for await (const conn of listener) {
-  Deno.copy(conn, conn);
+import { serve } from "https://deno.land/std@0.89.0/http/server.ts";
+const server = serve({ port: 8000 });
+console.log("http://localhost:8000/");
+
+for await (const req of server) {
+  req.respond({ body: "Hello World\n" });
 }
